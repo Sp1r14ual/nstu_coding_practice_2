28883d3940f9eda20341e268143759dc7c7fce8c
Sp1r14ual <sp1r14ual@gmail.com>
2022-09-23 18:21:12 +0700

    feat: Implemented full functionallity for rth_to_tenth and rth_to_tenth_fractional

--- Lab 1/main.py
+++ Lab 1/main.py
@@ -16,9 +16,16 @@ def rth_to_tenth(num, r, length):
         return num
     return num // 10 ** (length - 1) * r ** (length - 1) + rth_to_tenth(num % 10 ** (length - 1), r, length - 1)
 
+def rth_to_tenth_fractional(num, r, length):
+    if length == -1:
+        return num * r ** (length)
+    return num % 10 * r ** (length + 1) + rth_to_tenth_fractional(num // 10, r, length + 1)
+
 def solve(num, r):
-    pass
+    whole = floor(num)
+    whole_length = len(str(whole))
+    fraction = int(str(num)[str(num).index(".") + 1 : ])
+    fraction_length = len(str(fraction)) + 1
+    return rth_to_tenth(whole, r, whole_length) + rth_to_tenth_fractional(fraction, r, -fraction_length)
 
-
-
-print(tenth_to_rth_fractional(0.14, 2))
+print(solve(101.11, 2))
